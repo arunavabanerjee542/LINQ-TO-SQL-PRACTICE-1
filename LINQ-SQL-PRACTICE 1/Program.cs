27fa121de1912d9b6afedcf2431907dcb44933d4
Arunava Banerjee <Arunava Banerjee@DESKTOP-RBAIMPE>
2020-12-31 06:44:23 -0800

    Practice

--- LINQ-SQL-PRACTICE 1/Program.cs
+++ LINQ-SQL-PRACTICE 1/Program.cs
@@ -10,6 +10,115 @@ namespace LINQ_SQL_PRACTICE_1
     {
         static void Main(string[] args)
         {
+            SampleDataContext db = new SampleDataContext();
+
+            // this returns employee by department 
+
+            string s = string.Empty;
+            foreach(var x in db.GetEmployeesByDepartment(1,ref s))
+            {
+                Console.WriteLine( x.FirstName + " " + x.LastName + " " + x.DepartmentId );
+            }
+
+            // code behind 
+
+            /*
+            Create procedure GetEmployeesByDepartment
+           @DepartmentId int,
+             @DepartmentName nvarchar(50) out
+              as
+          Begin
+         Select @DepartmentName = Name
+         from Departments where ID = @DepartmentId
+
+           Select* from Employees
+           where DepartmentId = @DepartmentId
+
+            End
+            */
+
+
+
+            // lazy loading 
+
+            foreach(var dept in db.Departments)
+            {
+                foreach(var emp in dept.Employees)
+                {
+                    Console.WriteLine(emp.FirstName + emp.LastName);
+                }
+
+            }
+
+
+            // eager loading 
+
+            //trick 1 :  Projection
+
+            
+
+
+
+
+
+            // projection
+
+
+            var res = db.Departments.Select(x => new
+            {
+                dept = x.Name,
+                empl = x.Employees
+
+            });
+
+            db.Log = Console.Out;
+
+            foreach(var x in res)
+            {
+                Console.WriteLine(x.dept);
+
+                foreach(var e in x.empl)
+                {
+                    Console.WriteLine(e.FirstName + " " + e.LastName);
+                }
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+                
+                
+                      
+            }
+                      
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
-    }
-}
+   
