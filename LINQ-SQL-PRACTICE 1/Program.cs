6aa21679a1dea3e25e427e5047ce32be018ee326
Arunava Banerjee <Arunava Banerjee@DESKTOP-RBAIMPE>
2020-12-31 04:04:04 -0800

    Add project files.

---
+++ LINQ-SQL-PRACTICE 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_SQL_PRACTICE_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
